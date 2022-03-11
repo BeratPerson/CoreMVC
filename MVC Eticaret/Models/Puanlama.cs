29d2d0c6f86bf9c048a52d01becbff8a4b89604d
BeratPerson <93787263+BeratPerson@users.noreply.github.com>
2022-03-11 14:10:26 +0300

    .

---
+++ MVC Eticaret/Models/Puanlama.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Eticaret.Models
+{
+    public class Puanlama
+    {
+        [Key]
+        public int Yorum_Id { get; set; }
+        public int urun_Id { get; set; }
+        public int urun_Puan { get; set; }
+        public string urun_Yorum { get; set; }
+        public string Yorum_Yapan_ad_Soyad { get; set; }
+        public string Yorum_Yapan_Mail { get; set; }
+
+    }
+}
